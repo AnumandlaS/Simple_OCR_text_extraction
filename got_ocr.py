727f75ec7181b15c155e81ef1a286d3c0715aded
AnumandlaS <annumandlasiri@gmail.com>
2024-10-02 19:32:03 +0530

    code for got_ocr2.0 model

---
+++ got_ocr.py
@@ -0,0 +1,13 @@
+from transformers import AutoModel, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('srimanth-d/GOT_CPU', trust_remote_code=True)
+model = AutoModel.from_pretrained('srimanth-d/GOT_CPU', trust_remote_code=True, low_cpu_mem_usage=True, use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+model = model.eval()
+#'srimanth-d/GOT_CPU':- I am using this as I got its refernce from the official page of got_ocr_model, in the comments.
+
+# input your test image
+image_file = 'ocr_test1.png'
+# plain texts - OCR
+res = model.chat(tokenizer, image_file, ocr_type='ocr')
+
+print(res)
